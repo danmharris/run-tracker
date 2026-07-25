0fc1f2e78369bf03b47ba0bf1e9dfb865fcc0068
Dan Harris <me@danmharris.com>
2026-07-25 17:09:41 +0100

    feat: store and retrieve run data from database

--- app.rb
+++ app.rb
@@ -65,15 +65,22 @@ class App < Roda
     r.on 'runs' do
       r.is do
         r.get do
-          @runs = Dir["#{runs_dir}/*"].map { File.basename(_1).sub(/[.]gpx$/, '') }
+          @runs = Run.all
           view('runs/index')
         end
 
         r.post do
           tempfile = tp.file('file')[:tempfile]
+          gpx = GPX::GPXFile.new(gpx_file: tempfile)
           uuid = SecureRandom.uuid
+          Run.create(
+            uuid: uuid,
+            timestamp: gpx.tracks.first.points.first.time,
+            distance: gpx.distance,
+            duration: gpx.duration
+          )
           FileUtils.mv(tempfile.path, File.join(runs_dir, "#{uuid}.gpx"))
-          r.redirect('/runs')
+          r.redirect("/runs/#{uuid}")
         end
       end
 
@@ -82,6 +89,8 @@ class App < Roda
       end
 
       r.on String do |id|
+        next unless @run = Run.first(uuid: id)
+
         gpx = GPX::GPXFile.new(gpx_file: File.join(runs_dir, "#{id}.gpx"))
 
         r.get 'geojson' do
@@ -101,9 +110,6 @@ class App < Roda
         end
 
         r.get true do
-          @time = gpx.tracks.first.points.first.time
-          @distance = gpx.distance
-          @duration = gpx.duration
           @bounds = gpx.tracks.first.bounds
           @geojson_url = "#{r.path}/geojson"
 
